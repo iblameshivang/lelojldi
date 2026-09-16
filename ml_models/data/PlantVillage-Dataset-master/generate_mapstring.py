e049d95befab22e487616c33438d6488818e98f4
shiva02-coder <shivangmanhas2007@gmail.com>
2026-09-17 03:25:05 +0530

    Keep ml_models clean in git repository and sync project features

--- ml_models/data/PlantVillage-Dataset-master/generate_mapstring.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-import os
-
-for k in os.listdir("lmdb"):
-	print "/home/mohanty/data/final_dataset/lmdb/"+k
--- ml_models/data/PlantVillage-Dataset-master/scripts/run_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for k in `ls generate_data*`
-do	
-	echo $k
-	sbatch $k
-done
